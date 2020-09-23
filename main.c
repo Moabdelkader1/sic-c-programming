a3ea16d87f06e55dd74a75a9b73b24cf0e17cdb3
Moabdelkader1 <moabdelkader123@gmail>
2020-09-23 14:40:22 +0200

    i am adding main.c file

--- main.c
+++ main.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int num;
-    int sum=0;
-    printf("please enter a number\n");
-    scanf("%d",&num);
-    for(int i=1;i<num;i++){
-        if(num%i==0){
-            sum=sum+i;
-            }
-
+int sum(int arr[],int num){
+    int sum = 0;
+    for (int i =0; i < num; i++){
+        sum+= arr[i];
     }
-    if(sum==num){
-            printf("the number is perfect\n");
-
-          }
-          else{
-            printf("the number is not perfect\n");
-
-        }
+    printf("\nThe sum of non duplicated numbers is %d", sum);
     return 0;
-
 }
+
+int main(){
+
+ int counter=0;
+ int i,j;
+ int arr1[500];
+ int arr2[500];
+ printf("please enter 20 numbers ");
+ for( i=0;i<20;i++){
+    scanf("%d",&arr1[i]);
+ }
+ for( i=0;i<20;i++){
+
+    for( j=0;j<counter;j++){
+        if(arr1[i]==arr2[j])
+            break;
+        }
+      if(j==counter){
+        arr2[counter]=arr1[i];
+        counter++;
+      }
+    }
+    printf("the numbers of the unique array are:  ");
+    for(i=0;i<counter;i++){
+        printf("%d ",arr2[i]);
+    }
+
+        return sum(arr2,counter);
+ }
+
+
+
+
+
+
+
